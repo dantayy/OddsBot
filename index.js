fbfa85e773de70b20bec77c730376218613b6a28
dantayy <nvm6501@rit.edu>
2020-06-23 18:26:53 -0400

    Began reimplementation of redisScanHelper utilizing js Promises, next step is moving case specific code into .then blocs instead of being a parameter of the function, end goal is to fix bugs relating to async code execution

--- index.js
+++ index.js
@@ -49,24 +49,20 @@ function slackSlashCommand(req, res, next) {
         involvedParties.forEach(id => {
           console.log("Here's the id being inspected: " + id + " and for reference here's the initiator's id: " + req.body.user_id + ", and the challenged id: " + challengedID);
           if (id === req.body.user_id || id === challengedID) {
-            if (!responseSent) {
-              responseSent = true;
-              res.status(200).json({
-                text: "People can only have one outstanding challenge at a time, so you are they must resolve it first or cancel with /cancelodds",
-              });
-            }
+            res.status(200).json({
+              text: "People can only have one outstanding challenge at a time, so you or they must resolve it first or cancel with /cancelodds",
+            });
+            responseSent = true;
           }
         })
         return responseSent;
       });
       // handle bad case of a user trying to challenge themselves
       if (req.body.user_id === challengedID) {
-        if (!responseSent) {
-          responseSent = true;
-          res.status(200).json({
-            text: "You can't challenge yourself numbnuts!",
-          });
-        }
+        res.status(200).json({
+          text: "You can't challenge yourself numbnuts!",
+        });
+        responseSent = true;
         return;
       }
       // create a new hash with fields for all pertinant odds challenge data in redis by combining the initaitor and challenged ids
@@ -76,22 +72,18 @@ function slackSlashCommand(req, res, next) {
       // redisClient.hset(initiatorChallengedHash, 'challenged', challengedID, redis.print);
       redisClient.hset(initiatorChallengedHash, 'initiatorOdds', 0, redis.print);
       redisClient.hset(initiatorChallengedHash, 'challengedOdds', 0, redis.print);
-      if (!responseSent) {
-        responseSent = true;
-        res.status(200).json({ // send back the successful response if all has gone well
-          response_type: "in_channel",
-          text: "<@" + challengedID + ">, you've been challenged!  Submit your upper bounds now with /setodds or cancel the challenge with /cancelodds",
-        });
-      }
+      res.status(200).json({ // send back the successful response if all has gone well
+        response_type: "in_channel",
+        text: "<@" + challengedID + ">, you've been challenged!  Submit your upper bounds now with /setodds or cancel the challenge with /cancelodds",
+      });
+      responseSent = true;
       return;
     }
     else { // bad case of no user specified
-      if (!responseSent) {
-        responseSent = true;
-        res.status(200).json({
-          text: "You need to specify who you're challenging!"
-        });
-      }
+      res.status(200).json({
+        text: "You need to specify who you're challenging!"
+      });
+      responseSent = true;
       return;
     }
   }
@@ -109,30 +101,26 @@ function slackSlashCommand(req, res, next) {
         }
         if (upperLimit && upperLimit > 1 && upperLimit < Number.MAX_VALUE) { // check that there's a valid number in the text
           redisClient.hset(hash, 'upperLimit', upperLimit, redis.print);
-          if (!responseSent) {
-            responseSent = true;
-            res.status(200).json({
-              response_type: "in_channel",
-              text: "<@" + req.body.user_id + ">, Has set their upper limit to " + upperLimit + ", now both they and the challenge initator must enter a value betweeen 1 and that number with /commitodds",
-            });
-          }
+          res.status(200).json({
+            response_type: "in_channel",
+            text: "<@" + req.body.user_id + ">, Has set their upper limit to " + upperLimit + ", now both they and the challenge initator must enter a value betweeen 1 and that number with /commitodds",
+          });
+          responseSent = true;
         } else {
-          if (!responseSent) {
-            responseSent = true;
-            res.status(200).json({  // case of a bad value
-              text: "You need enter a valid integer!"
-            });
-          }
+          res.status(200).json({  // case of a bad value
+            text: "You need enter a valid integer!"
+          });
+          responseSent = true;
         }
       }
       return responseSent;
     });
     // this user was not challenged, tell them so
     if (!responseSent) {
-      responseSent = true;
       res.status(200).json({
         text: "You haven't been challenged!"
       });
+      responseSent = true;
     }
     return;
   }
@@ -148,70 +136,74 @@ function slackSlashCommand(req, res, next) {
             odds = req.body.text.match(`[0-9]+`)[0];
             console.log("First number grabbed to be the odds: " + odds);
           }
-          if (odds && odds >= 1 && odds <= redisClient.hget(hash, `upperLimit`)) { // check that there's a valid number in the text
-            let initiatorOdds = redisClient.hget(hash, `initiatorOdds`);
-            let challengedOdds = redisClient.hget(hash, `challengedOdds`);
-            // set the odds of the corresponding involved user only if they haven't put one in already
-            let oddsAlreadyEntered = false;
-            if (i === 0) { // initiator
-              if (initiatorOdds === 0) {
-                redisClient.hset(hash, 'initiatorOdds', odds, redis.print);
-              } else {
-                oddsAlreadyEntered = true;
-              }
-            } else { // challenged
-              if (challengedOdds === 0) {
-                redisClient.hset(hash, 'challengedOdds', odds, redis.print);
-              } else {
-                oddsAlreadyEntered = true;
+          redisClient.hget(hash, `upperLimit`, (err, reply) => { // wait to get the upperLimit back from redis, then execute based on that value returned
+            if (err)
+              throw err;
+            else {
+              console.log("Odds submitted: " + odds);
+              console.log("Reply from hget of upperLimit: " + reply);
+              if (odds && odds >= 1 && odds <= reply) { // check that there's a valid number in the text
+                let initiatorOdds;
+                let challengedOdds;
+                redisClient.hget(hash, `initiatorOdds`, (err, iOdds) => { if (err) { throw err; } else { initiatorOdds = iOdds; } });
+                redisClient.hget(hash, `challengedOdds`, (err, cOdds) => { if (err) { throw err; } else { challengedOdds = cOdds; } });
+                // set the odds of the corresponding involved user only if they haven't put one in already
+                let oddsAlreadyEntered = false;
+                if (i === 0) { // initiator
+                  if (initiatorOdds === 0) {
+                    redisClient.hset(hash, 'initiatorOdds', odds, redis.print);
+                  } else {
+                    oddsAlreadyEntered = true;
+                  }
+                } else { // challenged
+                  if (challengedOdds === 0) {
+                    redisClient.hset(hash, 'challengedOdds', odds, redis.print);
+                  } else {
+                    oddsAlreadyEntered = true;
+                  }
+                }
+                // update these vars with the new values
+                redisClient.hget(hash, `initiatorOdds`, (err, iOdds) => { if (err) { throw err; } else { cosnole.log(iOdds);  initiatorOdds = iOdds; } });
+                redisClient.hget(hash, `challengedOdds`, (err, cOdds) => { if (err) { throw err; } else { console.log(cOdds);  challengedOdds = cOdds; } });
+                console.log("Initiator odds: " + initiatorOdds);
+                console.log("Challenged odds: " + challengedOdds);
+                // check to see if both parties have their odds entered.  If they do, send back the results and delete this hash from the db
+                if (initiatorOdds !== 0 && challengedOdds !== 0) {
+                  redisClient.del(hash);
+                  res.status(200).json({
+                    response_type: "in_channel",
+                    text: "Both parties have submitted their odds!  <@" + involvedParties[0] + "> has entered " + initiatorOdds + ", and <@" + involvedParties[1] + "> has entered " + challengedOdds + ".  Do with that information what you will...",
+                  });
+                  responseSent = true;
+                } else if (oddsAlreadyEntered) { // case of user having already entered valid odds previously
+                  res.status(200).json({
+                    text: "You already entered your odds!",
+                  });
+                  responseSent = true;
+                } else { // default successful odds committed message
+                  res.status(200).json({
+                    text: "Odds entered successfully, waiting on opponent...",
+                  });
+                  responseSent = true;
+                }
+              } else { // case of a bad value
+                res.status(200).json({
+                  text: "You need enter a valid integer (Reminder that the upper limit is " + reply + ")",
+                });
+                responseSent = true;
               }
             }
-            // update these vars with the new values
-            initiatorOdds = redisClient.hget(hash, `initiatorOdds`);
-            challengedOdds = redisClient.hget(hash, `challengedOdds`);
-            // check to see if both parties have their odds entered.  If they do, send back the results and delete this hash from the db
-            if (initiatorOdds !== 0 && challengedOdds !== 0) {
-              redisClient.del(hash);
-              if (!responseSent) {
-                responseSent = true;
-                res.status(200).json({
-                  response_type: "in_channel",
-                  text: "Both parties have submitted their odds!  <@" + involvedParties[0] + "> has entered " + initiatorOdds + ", and <@" + involvedParties[1] + "> has entered " + challengedOdds + ".  Do with that information what you will...",
-                });
-              }
-            } else if (oddsAlreadyEntered) { // case of user having already entered valid odds previously
-              if (!responseSent) {
-                responseSent = true;
-                res.status(200).json({
-                  text: "You already entered your odds!",
-                });
-              }
-            } else { // default successful odds committed message
-              if (!responseSent) {
-                responseSent = true;
-                res.status(200).json({
-                  text: "Odds entered successfully, waiting on opponent...",
-                });
-              }
-            }
-          } else { // case of a bad value
-            if (!responseSent) {
-              responseSent = true;
-              res.status(200).json({
-                text: "You need enter a valid integer (Reminder that the upper limit is " + redisClient.hget(hash, `upperLimit`) + ")",
-              });
-            }
-          }
+          });
         }
       }
       return responseSent;
     });
-    // this user was not challenged, tell them so
+    // final case of no active challenges with this user
     if (!responseSent) {
-      responseSent = true;
       res.status(200).json({
-        text: "You haven't been challenged!"
+        text: "You have no active challenges right now!"
       });
+      responseSent = true;
     }
     return;
   }
@@ -223,52 +215,61 @@ function slackSlashCommand(req, res, next) {
         console.log("Here's the id being inspected: " + id + " and for reference here's the canceller's id: " + req.body.user_id);
         if (id === req.body.user_id) {
           redisClient.del(hash);
-          if (!responseSent) {
-            responseSent = true;
-            res.status(200).json({
-              response_type: `in_channel`,
-              text: "<@" + req.body.user_id + "> has cancelled their challenge.",
-            });
-          }
+          res.status(200).json({
+            response_type: `in_channel`,
+            text: "<@" + req.body.user_id + "> has cancelled their challenge.",
+          });
+          responseSent = true;
         }
       })
       return responseSent;
     });
+    console.log("Response sent status: " + responseSent);
     // this user was not challenged, tell them so
     if (!responseSent) {
-      responseSent = true;
       res.status(200).json({
-        text: "You have no active challenges right now.",
+        text: "You have no active challenges right now!",
       });
+      responseSent = true;
     }
     return;
   }
 }
 
 // helper function for scanning in the db that the redis client is attached to
-let redisScanHelper = (func) => {
+let redisScanHelper = async (func) => {
   let cursor = 0; // iterator value for the scan function
-  let keyUsed = false; // flag to break the loop if the key being looked at was used 
+  // let keyUsed = false; // flag to break the loop if the key being looked at was used 
   // loop through the db using the iterator and check the groups of hashes it returns with whatever function was passed to this
+  // do {
+    
+  //   redisClient.scan(cursor, (err, reply) => {
+  //     if (err)
+  //       throw err;
+  //     else {
+  //       cursor = reply[0]; // new cursor value is the first element
+  //       console.log("Cursor value: " + cursor);
+  //       let keys = reply[1]; // array of keys is second element
+  //       console.log("Keys at the current cursor: " + keys);
+  //       if (keys) {
+  //         for (let i = 0; i < keys.length; i++) {
+  //           keyUsed = func(keys[i]);
+  //           if (keyUsed) {
+  //             console.log("Key has been used!");
+  //             break;
+  //           }
+  //         }
+  //         //keys.forEach(func);
+  //       }
+  //     }
+  //   });
+  // } while (cursor && cursor !== 0 && !keyUsed);
+  // console.log("Done looping through keys");
+  let scanPromises = new Promise<Number>[];
   do {
-    redisClient.scan(cursor, (err, reply) => {
-      if (err)
-        throw err;
-      else {
-        cursor = reply[0]; // new cursor value is the first element
-        console.log("Cursor value: " + cursor);
-        let keys = reply[1]; // array of keys is second element
-        console.log("Keys at the current cursor: " + keys);
-        if (keys) {
-          for (let i = 0; i < keys.length; i++) {
-            keyUsed = func(keys[i]);
-            if (keyUsed) {
-              break;
-            }
-          }
-          //keys.forEach(func);
-        }
-      }
-    });
-  } while (cursor && cursor !== 0 && !keyUsed);
+    const [newCursor, keys] = await redisClient.scan(cursor);
+    cursor = newCursor;
+    scanPromises = [...scanPromise, ...keys];
+  } while (cursor && cursor !== 0);
+  return Promise.all(scanPromises).catch((err) => { throw err; });
 }
